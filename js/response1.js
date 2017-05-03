1a2860d432d660c80fd25ddf0779b5462f072e21
urzp <urzp002@mail.ru>
2017-05-03 12:23:43 +0300

    add info about set and get at the omdbapi.com

---
+++ js/response1.js
@@ -0,0 +1,71 @@
+http://www.omdbapi.com/?s=tiger&type=movie&page=1
+
+{"Search":[
+    {"Title":"Crouching Tiger, Hidden Dragon",
+     "Year":"2000",
+     "imdbID":"tt0190332",
+     "Type":"movie",
+     "Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BNDdhMzMxOTctNDMyNS00NTZmLTljNWEtNTc4MDBmZTYxY2NmXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"
+    },
+    
+    {"Title":"Ek Tha Tiger",
+    "Year":"2012",
+    "imdbID":"tt2016894",
+     "Type":"movie",
+     "Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BZTEyMTlmYTUtMDE0My00NWJmLWJlZmEtNzhjNGI0Mzk0NTA4XkEyXkFqcGdeQXVyNTkzNDQ4ODc@._V1_SX300.jpg"
+    },
+    
+    {"Title":"Crouching Tiger, Hidden Dragon: Sword of Destiny",
+     "Year":"2016",
+     "imdbID":"tt2652118",
+     "Type":"movie",
+     "Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTA2MzM5NTk4NTBeQTJeQWpwZ15BbWU4MDk0NjgyNDcx._V1_SX300.jpg"
+    },
+    
+    {"Title":"The Tiger and the Snow",
+     "Year":"2005",
+     "imdbID":"tt0419198",
+     "Type":"movie",
+     "Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTA0OTYyMDQzMDReQTJeQWpwZ15BbWU3MDk0OTU0NDE@._V1_SX300.jpg"
+    },
+    
+    {"Title":"What's Up, Tiger Lily?",
+     "Year":"1966",
+     "imdbID":"tt0061177",
+     "Type":"movie",
+     "Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMjAzNTQ2MTczMF5BMl5BanBnXkFtZTYwMTcyMjg5._V1_SX300.jpg"
+    },
+    
+    {"Title":"Dragon Tiger Gate",
+     "Year":"2006",
+     "imdbID":"tt0482930",
+     "Type":"movie",
+     "Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTEyMTk0OTg4NTleQTJeQWpwZ15BbWU4MDI2OTI1NDEx._V1_SX300.jpg"
+    },
+    
+    {"Title":"Sinbad and the Eye of the Tiger",
+     "Year":"1977",
+     "imdbID":"tt0076716",
+     "Type":"movie",
+     "Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BOTI2MzU4NjM4NV5BMl5BanBnXkFtZTgwNTkwNzYzNTE@._V1_SX300.jpg"
+    },
+    
+    {"Title":"Glass Tiger",
+     "Year":"2001",
+     "imdbID":"tt0257290",
+     "Type":"movie",
+     "Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BYzg0MmI3NjUtZjZkYy00MmJmLTljMTItZGVjN2VkMGFhMDg3XkEyXkFqcGdeQXVyMzc1MDUyOTY@._V1_SX300.jpg"},
+    
+    {"Title":"Save the Tiger",
+     "Year":"1973",
+     "imdbID":"tt0070640",
+     "Type":"movie",
+     "Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTlkOThjY2QtZjc5ZS00MjRmLWI4YjEtMTNkNDE1ZGYxMDhhXkEyXkFqcGdeQXVyMTAwMzUyOTc@._V1_SX300.jpg"},
+    
+    {"Title":"Glass Tiger 2",
+     "Year":"2006",
+     "imdbID":"tt0476508",
+     "Type":"movie",
+     "Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BYTkzZWVlNzItYzA2Yi00YTlkLWFlZTItNDU1MTE5OTcwZTI5XkEyXkFqcGdeQXVyMzQ0ODIzMTg@._V1_SX300.jpg"
+    }
+],"totalResults":"521","Response":"True"}
---
+++ js/response2.js
@@ -0,0 +1,31 @@
+{
+    "Title":"Crouching Tiger, Hidden Dragon",
+    "Year":"2000",
+    "Rated":"PG-13",
+    "Released":"12 Jan 2001",
+    "Runtime":"120 min",
+    "Genre":"Action, Adventure, Fantasy",
+    "Director":"Ang Lee",
+    "Writer":"Du Lu Wang (book), Hui-Ling Wang (screenplay), James Schamus (screenplay), Kuo Jung Tsai (screenplay)",
+    "Actors":"Yun-Fat Chow, Michelle Yeoh, Ziyi Zhang, Chen Chang",
+    "Plot":"In 19th century Qing Dynasty China, a warrior gives his sword, Green Destiny, to his lover to deliver to safe keeping, but it is stolen, and the chase is on to find it. The search leads to the House of Yu where the story takes on a whole different level.",
+    "Language":"Mandarin",
+    "Country":"Taiwan, Hong Kong, USA, China",
+    "Awards":"Won 4 Oscars. Another 96 wins & 128 nominations.",
+    "Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BNDdhMzMxOTctNDMyNS00NTZmLTljNWEtNTc4MDBmZTYxY2NmXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+    "Ratings":[
+                {"Source":"Internet Movie Database","Value":"7.9/10"},
+                {"Source":"Rotten Tomatoes","Value":"97%"},
+                {"Source":"Metacritic","Value":"93/100"}
+            ],
+    "Metascore":"93",
+    "imdbRating":"7.9",
+    "imdbVotes":"224,566",
+    "imdbID":"tt0190332",
+    "Type":"movie",
+    "DVD":"05 Jun 2001",
+    "BoxOffice":"N/A",
+    "Production":"Sony Pictures Classics",
+    "Website":"http://www.crouchingtiger.com",
+    "Response":"True"
+}
