574d35bba9d28e56b4a06655008fde0203a0e02b
urzp <urzp002@mail.ru>
2017-05-07 00:05:57 +0300

    still need for respose scroll

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,37 +1,9 @@
 $(function(){
-    var movie_data = {
-            "Title":"Crouching Tiger, Hidden Dragon",
-            "Year":"2000",
-            "Rated":"PG-13",
-            "Released":"12 Jan 2001",
-            "Runtime":"120 min",
-            "Genre":"Action, Adventure, Fantasy",
-            "Director":"Ang Lee",
-            "Writer":"Du Lu Wang (book), Hui-Ling Wang (screenplay), James Schamus (screenplay), Kuo Jung Tsai (screenplay)",
-            "Actors":"Yun-Fat Chow, Michelle Yeoh, Ziyi Zhang, Chen Chang",
-            "Plot":"In 19th century Qing Dynasty China, a warrior gives his sword, Green Destiny, to his lover to deliver to safe keeping, but it is stolen, and the chase is on to find it. The search leads to the House of Yu where the story takes on a whole different level.",
-            "Language":"Mandarin",
-            "Country":"Taiwan, Hong Kong, USA, China",
-            "Awards":"Won 4 Oscars. Another 96 wins & 128 nominations.",
-            "Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BNDdhMzMxOTctNDMyNS00NTZmLTljNWEtNTc4MDBmZTYxY2NmXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-            "Ratings":[
-                        {"Source":"Internet Movie Database","Value":"7.9/10"},
-                        {"Source":"Rotten Tomatoes","Value":"97%"},
-                        {"Source":"Metacritic","Value":"93/100"}
-                    ],
-            "Metascore":"93",
-            "imdbRating":"7.9",
-            "imdbVotes":"224,566",
-            "imdbID":"tt0190332",
-            "Type":"movie",
-            "DVD":"05 Jun 2001",
-            "BoxOffice":"N/A",
-            "Production":"Sony Pictures Classics",
-            "Website":"http://www.crouchingtiger.com",
-            "Response":"True"
-        }
     
-    
+    function new_row_movies(movie){
+        new_row();
+        fill_last_row(movie);
+    }
     var new_row = function(){
         $(".content").append('<div class="row"></div>') 
         $(".row:last").append('<div class="image">')
@@ -47,7 +19,6 @@ $(function(){
         $(".Stars:last").append('<p>Stars:<span></span></p>')    
         $(".row:last").append('<div class="clear-fix"></div>')
     }
-    
     var fill_last_row = function(movie){
         movie = trasform_move_data(movie);
         $(".image:last").append(movie.Poster)
@@ -58,10 +29,9 @@ $(function(){
         $(".writers:last > p > span").append(movie.Writer)
         $(".Stars:last > p > span").append(movie.Actors)
     }
-    
     var trasform_move_data = function (movie){
         new_data = {};
-        new_data.Poster = '<img src="' + movie.Poster + '" alt="">'
+        new_data.Poster = '<img src="' + movie.Poster + '" alt="img none">'
         new_data.Title = '<h3>' + movie.Title + ' <span>(' + movie.Year +')</span></h3>' 
         new_data.genre = movie.Runtime + ' ' + movie.Genre.replace(/\,/gi," |" ) + " - " + movie.Released;
         new_data.Plot = movie.Plot;
@@ -69,13 +39,60 @@ $(function(){
         new_data.Writer = movie.Writer
         new_data.Actors = movie.Actors
         return new_data;
+    }        
+    var get_movies = function(find_text,page){
+        var movies = [];
+        $.ajax({
+            url: "http://www.omdbapi.com/?",
+            type: "GET",
+            data: ({s:find_text,type:"movie",page:page}),
+            dataType: "json",
+            beforeSend: functionBefore,
+            success: functionSuccess
+        })
         
+        function functionSuccess (data){
+            //data = JSON.parse(data);
+            data = data["Search"];
+            
+            data.forEach(function(item){
+               movies.push(item["imdbID"]); 
+                get_info(item["imdbID"]); 
+            });
+
+        }
+        
+        function functionBefore(){
+            $('footer > img').css("display", "block")
+        }
+        
+        
+        function get_info(id){
+            $.ajax({
+                url: "http://www.omdbapi.com/?",
+                type: "GET",
+                data: ({i:id,plot:"full"}),
+                dataType: "json",
+                beforeSend: functionBefore,
+                success: get_info_Success
+            })
+            
+            function get_info_Success(info){
+                $('footer > img').css("display", "none")
+                new_row_movies(info);
+            }
+            
+        }
         
     }
     
-    new_row();
-    fill_last_row(movie_data);
-    
+
+    get_movies(5);
+    get_movies(6);
+    get_movies(7);
+    get_movies(8);
+
+     
 })
 
 
